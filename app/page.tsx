2461c8493ad9cade2ae324b20f915f5066a83db0
its_jordan <s.schultzjordan.jordan@gmail.com>
2024-03-23 21:33:35 -0400

    added funny homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,64 @@
 'use client';
 
+import Image from 'next/image';
+
+const plusleArray = [
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+  'plusle',
+];
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className='flex flex-col justify-center items-center'>
+        {}
+        {plusleArray.map((data, index) => {
+          return (
+            <Image
+              className='plusle-img'
+              src={'/plusle.gif'}
+              key={index}
+              data-pokemon={data}
+              width={index * 32}
+              height={index * 32}
+              alt='plusle and minun'></Image>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
