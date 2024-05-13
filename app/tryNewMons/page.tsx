1f99f54e37a43d058de3d5a5e2786eec762116f0
its_jordan <s.schultzjordan.jordan@gmail.com>
2024-05-12 21:46:14 -0400

    added json file for next season's mons

---
+++ app/tryNewMons/page.tsx
@@ -0,0 +1,26 @@
+import mons from '@/data/allmons2.json';
+
+export async function tryMonsAPI(mon: string[]) {
+  return mon.forEach(async (mon) => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${mon}`);
+    if (res.ok) {
+      const data = await res.json();
+      const name = await data.name;
+      console.log(data);
+      return name;
+    } else {
+      console.log(mon);
+      return mon;
+    }
+  });
+}
+
+export default async function TryMon() {
+  tryMonsAPI(mons);
+  return (
+    <div>
+      Trying every pokemon against the pokeapi. Check Console for more
+      information.
+    </div>
+  );
+}
